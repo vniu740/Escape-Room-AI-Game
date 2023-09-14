11205e5ac96f66c22956b02ba5c39758f00f571d
VENXIA <vniu740>
2023-09-14 16:28:21 +1200

    refactor: changed load file for startup
    This commit refactors code I previously pushed. I had changed the file back to the original startup code as I had accidently pushed my testing code

--- src/main/java/nz/ac/auckland/se206/App.java
+++ src/main/java/nz/ac/auckland/se206/App.java
@@ -43,11 +43,10 @@ public class App extends Application {
    */
   @Override
   public void start(final Stage stage) throws IOException {
-    Parent root = loadFxml("lab");
+    Parent root = loadFxml("room");
     scene = new Scene(root, 600, 470);
     stage.setScene(scene);
     stage.show();
     root.requestFocus();
   }
-
 }
